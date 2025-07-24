aebb4c0b5e1b0ed75fdcea2d2d669c47b4ddf0ef
caludio1985 <claudiogabrielcastillo1985@gmail.com>
2025-07-24 15:14:43 -0300

    hola

--- motor/full_bridge.asm
+++ motor/full_bridge.asm
@@ -1,14 +1,12 @@
-LISTP=16F887
-#INCLUDE<P16F887.INC>		
+ LISTP=16F887
+#INCLUDE<P16F887.INC>
+		
+;*******************************
+;	Oscilador interno
 ;*******************************
 __CONFIG    _CONFIG1, _LVP_OFF & _FCMEN_OFF & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_OFF & _WDT_OFF & _INTRC_OSC_NOCLKOUT
 __CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
-;************************************************
-;OSCILADOR EXTERNO 16MHZ
-;************************************************
-;__CONFIG    _CONFIG1, _LVP_OFF & _FCMEN_OFF & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_OFF & _PWRTE_ON & _WDT_OFF & _HS_OSC
-;__CONFIG    _CONFIG2, _WRT_OFF & _BOR21V 
-;************************************************
+;*******************************
 
 BANCO_1	MACRO
 			BCF		STATUS,RP1
@@ -53,7 +51,6 @@ INICIO
 			BANCO_2
 			MOVLW	B'01110101'
 			MOVWF	OSCCON			
-			;BCF		OSCCON,0
 ;******************************************************
 ;********* Configuro pines de entrada *****************
 ;******************************************************
@@ -62,19 +59,20 @@ INICIO
 			BSF		TRISB,2
 			MOVLW	B'00000001'
 			MOVWF	TRISA
+			BCF		OPTION_REG,7
 ;*****************************************************
 ;******** Configuro TIMER-2 **************************
 ;*****************************************************
-			BANCO_2
+NUEVO		BANCO_2
 			MOVLW	D'255'
 			MOVWF	PR2
 			BANCO_1
 			CLRF	CCPR1L
-			MOVLW	B'00111100'
+			MOVLW	B'00001100'
 			MOVWF	CCP1CON
 			BCF		PIR1,1
 			BSF		T2CON,2
-TIME	BTFSS	PIR1,1
+TIME		BTFSS	PIR1,1
 			GOTO	TIME
 			BCF		PIR1,1
 ;****************************************************
@@ -89,14 +87,14 @@ TIME	BTFSS	PIR1,1
 ;******** configuro modulo ADC ****************
 ;****************************************************
 			BANCO_2
-			MOVLW	B'00000000' ;JUSTIFICACION A LA IZQUIERDA
+			MOVLW	B'00000000' ;JUSTIF. A LA IZQ.
 			MOVWF	ADCON1
 			BANCO_1
 			MOVLW	B'11000001'	; CANAL-1
 			MOVWF	ADCON0
-LOOP	BSF		ADCON0,GO	
+LOOP		BSF		ADCON0,GO	
 			CALL	DELAY
-STOP	BTFSC	ADCON0,GO
+STOP		BTFSC	ADCON0,GO
 			GOTO	STOP
 ;****************************************************
 ;******** CARGAR 10-BITS DE ADC A PWM ***************
@@ -104,63 +102,33 @@ STOP	BTFSC	ADCON0,GO
 			BANCO_1
 			MOVF	ADRESH,W	
 			MOVWF	CCPR1L		
-			BANCO_2
-			MOVF	ADRESL,W
-			ANDLW	0xC0
-			RRF		WREG,W
-			RRF		WREG,W
-			MOVWF	AUX
-			BANCO_1
-			MOVF	CCP1CON,W
-			ANDLW	B'11001111'
-			IORWF	AUX,W
-			MOVWF	CCP1CON
 ;****************************************************
 ;******** Sentido de giro ***************************
 ;****************************************************
 			BANCO_1
-			BTFSS	PORTB,2
-			GOTO	UNO
-			BTFSS	PORTB,0
-			GOTO	CUATRO
-			GOTO	CINCO	
-
-CINCO	
-			BANCO_1
-			MOVF	CCP1CON,W
-			ANDLW	0x3F
-			IORLW	0x40
+			BTFSC	PORTB,2
+			GOTO	NUEVO
+			BTFSC	PORTB,0
+			GOTO	IZQ
+			GOTO	DERECH	
+IZQ			MOVLW	B'01001100'
 			MOVWF	CCP1CON
 			GOTO	LOOP
-
-CUATRO
-			BANCO_1
-			MOVF	CCP1CON,W
-			ANDLW	0x3F
-			IORLW	0xC0
+DERECH		MOVLW	B'11001100'
 			MOVWF	CCP1CON
-			GOTO	LOOP
-
-UNO		
-			BANCO_1
-			CLRF	CCPR1L
-			CLRF	CCP1CON
-			GOTO	LOOP
+			GOTO 	LOOP
 
 ;-----------------------------------
 ;DELAY 20US
 ;-----------------------------------
-DELAY MOVLW	.8
+DELAY 		MOVLW	.8
 			MOVWF	PDel0
-PLoop0	
-			CLRWDT
+PLoop0		CLRWDT
 			DECFSZ	PDel0,1
 			GOTO	PLoop0
-PDelL1	
-			GOTO	PDelL2
-PDelL2	
-			CLRWDT	
+PDelL1		GOTO	PDelL2
+PDelL2		CLRWDT	
 			RETURN
 ;-----------------------------------
 			END
-;-------------------------		
+;-----------------------------------		
